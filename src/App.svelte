d06d3a5041dcc19153d4f3ed3c4d55cf5148a3bf
Ahmad Moghazi <moghaaazi@gmail.com>
2024-07-10 00:35:19 +0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  // import Swiper JS
+  import Swiper from 'swiper'
+  // import Swiper styles
+  import 'swiper/css'
+
+  const swiper = new Swiper('.swiper', {})
+</script>
+
+<header class="container">
+  <nav>
+    <div class="logo">Logo</div>
+
+    <div class="links">
+      <ul>
+        <li><a href="shop">Link 1</a></li>
+        <li><a href="shop">Link 2</a></li>
+        <li><a href="shop">Link 3</a></li>
+        <li><a href="shop">Link 4</a></li>
+      </ul>
+    </div>
+
+    <div class="buttons">
+      <button>Button 1</button>
+      <button>Button 1</button>
+      <button>Button 1</button>
+      <button>Button 1</button>
+    </div>
+  </nav>
+
+  <div class="hero">
+    <h1 class="text-red">Hero</h1>
+  </div>
+</header>
