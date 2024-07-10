66fcd5b5790eb709271938cb86cb0ce3ba08bd22
Ahmad Moghazi <moghaaazi@gmail.com>
2024-07-11 02:52:48 +0300

    .

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-import { vitePreprocess } from '@sveltejs/kit/vite'
-
 export default {
-  preprocess: vitePreprocess([
-    postcss({ plugins: { tailwindcss: {}, autoprefixer: {} } }),
-  ]),
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
